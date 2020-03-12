73814d6067561151644be23f5a631615ec65f5ac
unknown <sgbtech96@gmail.com>
2020-03-12 19:36:24 +0530

    ;ldjhgc

--- main.js
+++ main.js
@@ -31,27 +31,9 @@ app.post('/upload', upload.single('upload'), async (req, res) => {
 	res.status(400).send({ error: error.message })
 })
 
-app.get('/see', async (req, res) => {
-	const img = await imgs.find({})
-	var a = 0
-	img.forEach((i) => {
-		a += 1
-		const pixelSize = 256
-		var image = new Jimp(pixelSize, pixelSize, function (err, image) {
-	    let buffer = i.num
-	    for (var x = 0; x < pixelSize; x++) {
-	      	for (var y = 0; y < pixelSize; y++) {
-			      const offset = (y * pixelSize + x) * 4 // RGBA = 4 bytes
-			      buffer[offset    ] = x    // R
-			      buffer[offset + 1] = y    // G
-			      buffer[offset + 2] = 0    // B
-			      buffer[offset + 3] = 255  // Alpha
-	   		 }
-	  	}
-	})
-	image.write('/frames/pic' + a +'.png')
-	})
-	
+app.get('/see', (req, res) => {
+	const img = imgs.find({})
+	res.send(img)
 })
 
 app.listen(port, () => {
