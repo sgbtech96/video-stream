8ef79066fe113dce712033c149f47462a1cf628e
Siddhant Gandhi <43717462+sgbtech96@users.noreply.github.com>
2020-03-12 23:20:11 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('./imgs')
-mongoose.connect('mongodb+srv://sgbtech96:nptel@92@cluster0-hluvl.mongodb.net/test?retryWrites=true&w=majority/apurv', {
+mongoose.connect('mongodb+srv://sgbtech96:nptel@92@cluster0-hluvl.mongodb.net/test?retryWrites=true&w=majority', {
 	useNewUrlParser: true,
 	useCreateIndex: true
 })
